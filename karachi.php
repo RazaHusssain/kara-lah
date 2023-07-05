4913754eda2197543bc3508280d6ec0542b8dafa
RazaHusssain <138428277+RazaHusssain@users.noreply.github.com>
2023-07-05 23:30:33 +0500

    kemari

--- karachi.php
+++ karachi.php
@@ -4,7 +4,7 @@
 <title></title>
 </head>
 <body>
-
+<p>Karachi definition, a seaport in S Pakistan, near the Indus delta: former national capital; now capital of Sind province</p>
 <h1>karachi beautiful</h1>
 
 </body>
